40607df0bbfe1e42c2b3e345f868e97d563f768c
Mr. Ubong <ubongtech01@gmail.com>
2023-08-06 20:47:39 +0100

    Added API routes

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return CollegeRepository.Students;
+        }
+        [HttpGet("{id}")]
+        public Student GetStudentById(int id)
+        {
+            return CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+        }
+        public Student GetStudentByName(string name) 
+        {
+           return CollegeRepository.Students.Where(n => n.Name == name).FirstOrDefault();
+        }
+        public bool DeleteStudentById(int id) 
+        {
+          var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            CollegeRepository.Students.Remove(student);
+            return true;
+        }
+    }
+}
---
+++ Models/CollegeRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> students { get; set; } = new List<Student>()
+      {
+          new Student()
+          {
+              Id = 1,
+              Name = "Ubong",
+              Email = "ubong@gmail.com",
+              Address = "No 10 Ewet",
+          },
+          new Student()
+          {
+              Id = 2,
+              Name = "Ken",
+              Email = "ken@gmail.com",
+              Address = "N0 12 Oron",
+          }
+      };
+    }
+}
